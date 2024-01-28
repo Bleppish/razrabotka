f9c03f62e8183e337ed08c896aad3c85f80bf189
BlepAn <99560385+Bleppish@users.noreply.github.com>
2024-01-28 20:02:28 +0000

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+// src/models/User.js
+/**
+ * @typedef {Object} User
+ * @property {number} id - The unique identifier for the user.
+ * @property {string} name - The name of the user.
+ * @property {string} email - The email address of the user (must be unique).
+ */
+
+/**
+ * @typedef {import('@prisma/client').Prisma.UserCreateInput} UserCreateInput
+ */
+
+/**
+ * @typedef {import('@prisma/client').Prisma.UserUpdateInput} UserUpdateInput
+ */
+
+/**
+ * @typedef {import('@prisma/client').Prisma.UserWhereUniqueInput} UserWhereUniqueInput
+ */
