4a04eda97115a29df379bfb1c0beac192c8a7e92
BlepAn <99560385+Bleppish@users.noreply.github.com>
2024-01-28 19:38:51 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const UserController = require('./controllers/UserController');
 
+const app = express();
+const port = 3000;
+
+// Swagger Configuration
+const swaggerOptions = require('./swagger');
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
+app.use('/users', UserController);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
